c2b63892b72cff33024e23d19c39450082c19136
jason <jason.addie@gmail.com>
2014-10-22 17:29:02 +0400

    basic crosstab in person model working; now trying to get it to work in dataset

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -40,4 +40,80 @@ class Dataset
   end
 
 
+  def data_crosstab_analysis(row, col)
+    # get uniq values of col
+    rows = self.data.map{|x| x[row]}.uniq.sort
+    cols = self.data.map{|x| x[col]}.uniq.sort
+
+    results = []
+    data = {}
+
+    cols.each do |c|
+      puts "--------------------"
+      puts "c = #{c}"
+
+      # need to make sure the row value and c value are recorded as strings
+      # for if it is an int, the javascript function turns it into a decimal 
+      # (2 -> 2.0) and then comparisons do not work!
+      map = "
+        function() {
+           for (var i = 0; i < this.data.length; i++) {
+            emit('#{row}', { #{col}: '#{c}', count: 1 }); 
+           }
+        };
+      "
+
+      puts map
+      puts "---"
+
+
+      reduce = "
+        function(rowKey, columnValues) {
+          var result = { #{col}: '#{c}', count: 0 };
+
+          for (var i = 0; i < columnValues.length; i++) {
+            result.count += 1;
+          }
+          return result;
+        };
+      "
+
+
+      puts reduce
+      puts "---"
+
+      results << Dataset.where(:id => self.id).map_reduce(map, reduce).out(inline: true).to_a
+
+    end
+
+    # flatten the results
+    puts "++ results length was = #{results.length}"
+    results.flatten!
+    puts "++ results length = #{results.length}"
+
+puts results
+
+    # now put it all together
+    data[:row_header] = row.titlecase
+    data[:row_answers] = rows
+    data[:column_header] = col.titlecase
+    data[:column_answers] = cols
+
+    data[:counts] = []
+    rows.each do |r|
+      data_row = []
+      cols.each do |c|
+        data_match = results.select{|x| x['_id'].to_s == r.to_s && x['value'][col].to_s == c.to_s}
+        if data_match.present?
+          data_row << data_match.map{|x| x['value']['count']}.inject(:+)
+        else
+          data_row << 0
+        end
+      end
+      data[:counts] << data_row
+    end
+
+    return data
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -131,9 +131,9 @@ puts "---"
     end
 
     # flatten the results
-    puts "++ rseults length was = #{results.length}"
+    puts "++ results length was = #{results.length}"
     results.flatten!
-    puts "++ rseults length = #{results.length}"
+    puts "++ results length = #{results.length}"
 
 puts results
 
@@ -160,4 +160,77 @@ puts results
     return data
   end
 
+
+  def self.generate_stats5(row, col)
+    # get uniq values of col
+    cols = distinct(col).sort  
+    rows = distinct(row).sort
+
+    results = []
+    data = {}
+
+    cols.each do |c|
+      puts "--------------------"
+      puts "c = #{c}"
+
+      # need to make sure the row value and c value are recorded as strings
+      # for if it is an int, the javascript function turns it into a decimal 
+      # (2 -> 2.0) and then comparisons do not work!
+      map = "
+        function() {
+          emit(this.#{row}.toString(), { #{col}: '#{c}', count: 1 }); 
+        }  
+      "
+
+      puts map
+      puts "---"
+
+
+      reduce = "
+        function(key, values) {   
+          var result = { #{col}: '#{c}', count: 0 };
+          values.forEach(function(value) {
+            result.count += 1; 
+          });
+          return result;
+        }
+      "
+
+
+      puts reduce
+      puts "---"
+
+      results << where(col => c).map_reduce(map, reduce).out(inline: true).to_a
+
+    end
+
+    # flatten the results
+    puts "++ results length was = #{results.length}"
+    results.flatten!
+    puts "++ results length = #{results.length}"
+
+puts results
+
+    # now put it all together
+    data[:row_header] = row.titlecase
+    data[:row_answers] = rows
+    data[:column_header] = col.titlecase
+    data[:column_answers] = cols
+
+    data[:counts] = []
+    rows.each do |r|
+      data_row = []
+      cols.each do |c|
+        data_match = results.select{|x| x['_id'].to_s == r.to_s && x['value'][col].to_s == c.to_s}
+        if data_match.present?
+          data_row << data_match.map{|x| x['value']['count']}.inject(:+)
+        else
+          data_row << 0
+        end
+      end
+      data[:counts] << data_row
+    end
+
+    return data
+  end
 end
