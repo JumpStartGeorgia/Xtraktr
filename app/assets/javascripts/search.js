1fe4e1996810794ef1105059c244f5eae228a776
jason <jason.addie@gmail.com>
2014-12-17 14:07:33 +0400

    force asset compile

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
   $.extend( $.fn.dataTableExt.oStdClasses, {
       "sWrapper": "dataTables_wrapper form-inline"
@@ -12,22 +11,6 @@ $(document).ready(function(){
     "language": {
       "url": gon.datatable_i18n_url
     }
-    /*,
-    
-
-    "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>r>t<'row'<'col-sm-6'i><'col-sm-6'p>>",    
-    "sPaginationType": "bootstrap",
-    "bJQueryUI": true,
-    "bProcessing": true,
-    "bServerSide": true,
-    "bDestroy": true,
-    "bAutoWidth": false,
-    "sAjaxSource": $('#users-datatable').data('source'),
-    "oLanguage": {
-      "sUrl": gon.datatable_i18n_url
-    },
-    "aaSorting": [[2, 'desc']]
-    */
   });
 
 
