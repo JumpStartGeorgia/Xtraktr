f78085fec6e3468aa5d0db25fde65a48e4e1057f
jason <jason.addie@gmail.com>
2014-12-17 13:23:37 +0400

    ignore extra info when login with fb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    logger.debug "++++++++++== facebook start"
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      logger.debug "++++++++++== user persisted"
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      logger.debug "++++++++++== setting session"
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except('extra')
       logger.debug(request.env["omniauth.auth"])
       redirect_to new_user_session_url
     end
