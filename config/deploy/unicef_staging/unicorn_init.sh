6ed60bab630c98acb8d75589b4258f5cc0f606a9
jason <jason.addie@gmail.com>
2015-03-30 14:16:49 +0400

    fix path to staging

--- config/deploy/unicef_staging/unicorn_init.sh
+++ config/deploy/unicef_staging/unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 AS_USER=unicef-staging # name of user on server
 APP_ROOT=/home/unicef-staging/Unicef-Staging/current # path to application current folder
 # update the name of the enviroment at '-E _____' to production, staging, etc
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/deploy/staging/unicorn.rb -E staging"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/deploy/unicef_staging/unicorn.rb -E staging"
 ##################################
 
 PID=$APP_ROOT/tmp/pids/unicorn.pid
