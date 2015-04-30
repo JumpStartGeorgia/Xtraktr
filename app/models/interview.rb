7f8260e3b6eb5a2d012d3e3d256edbfb802b279a
jason <jason.addie@gmail.com>
2015-04-30 10:03:20 +0400

    re-org interview model

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,10 +1,12 @@
 class Interview
   include Mongoid::Document
-  #include Mongoid::Timestamps
+  include Mongoid::Timestamps
 
   #############################
 
   # belongs_to :user
+
+  #############################
 
     AGE_GROUP = { 1 => '17-24', 2 => '25-34', 3 => '35-44', 4 => '45-54', 5 => '55-64', 6 => 'above'}
     STATUS = { 1 => 'researcher',
@@ -16,31 +18,42 @@ class Interview
                7 => 'private_sector',
                8 => 'other' }
 
-    field :first_name, type: String
-    field :last_name, type: String
-    field :age_group, type: Integer
-    field :residence, type: String
-    field :email, type: String
-    field :affiliation, type: String
-    field :status, type: Integer
-    field :status_other, type: String
-    field :description, type: String
+  #############################
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :age_group, inclusion: { in: AGE_GROUP.keys }
-    validates :residence, presence: true
-    validates :email, presence: true
-    validates :affiliation, presence: true
-    validates :status, inclusion: { in: STATUS.keys }
-    validates_presence_of :status_other, :if => lambda { |o| o.status == 8 }
-    validates :terms, :numericality => { :equal_to => 1 }
+  field :first_name, type: String
+  field :last_name, type: String
+  field :age_group, type: Integer
+  field :residence, type: String
+  field :email, type: String
+  field :affiliation, type: String
+  field :status, type: Integer
+  field :status_other, type: String
+  field :description, type: String
 
-    attr_accessor :terms
-    attr_accessible :first_name, :last_name, :age_group, :residence,
-                    :email, :affiliation, :status, :status_other, :description, :terms
+  #############################
+
+  attr_accessor :terms
+  attr_accessible :first_name, :last_name, :age_group, :residence,
+                  :email, :affiliation, :status, :status_other, :description, :terms
 
 
+  #############################
+  ## Validations
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age_group, inclusion: { in: AGE_GROUP.keys }
+  validates :residence, presence: true
+  validates :email, presence: true
+  validates :affiliation, presence: true
+  validates :status, inclusion: { in: STATUS.keys }
+  validates_presence_of :status_other, :if => lambda { |o| o.status == 8 }
+  validates :terms, :numericality => { :equal_to => 1 }
+
+
+  #############################
+  ## Scopes
+  
   def self.test
     puts STATUS.inspect
   end
