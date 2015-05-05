ff427bb476cbbec648cdffcfa389148e36c56798
antarya <antarya@gmail.com>
2015-05-05 13:48:04 +0400

    Category filter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -96,9 +96,40 @@ $(document).ready(function(){
       }
     });
 
-  $('.category-picker .selector').click(function(){
-    var t = $(this);
+  $('.category[data-filter=category] .selector').click(function(e){
+    var t = $(this)
+    t.toggleClass('open');
+    var open = t.hasClass('open');
+    if(open)
+    {
+      $(document).on('click.category',function(){
+         t.removeClass('open');
+         t.parent().find('ul').toggle();
+         $(document).off('click.category');
+      });
+    }
+    else $(document).off('click.category');
     t.parent().find('ul').toggle();
+    e.stopPropagation();
+  });  
+
+  $('.category[data-filter=category] ul li').click(function(e){
+    var t = $(this);
+    var has = t.hasClass('active');
+    var v = has ? 'none' : t.attr('data-filter-value');
+    $('.category[data-filter=category] ul li').removeClass('active');  
+    var selector = $('.category[data-filter=category] .selector');
+    var img  = selector.find('.img');
+    var span  = selector.find('span');
+    selector.removeClass('open');
+    img.removeClass(img.attr('data-selected')).addClass(v).attr('data-selected', v).attr('title', (has ? span.attr('data-text-none') : t.find('.img').attr('title')));
+    span.text(span.attr('data-text-' + (has ? v : 'selected')));
+    if(!has) t.addClass('active');
+    
+    t.parent().toggle();
+    
+    $(document).off('click.category');
+    e.stopPropagation();
   });  
 
 });
