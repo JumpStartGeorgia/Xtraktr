33a79a5c957806a49976d8b3f159097c1c70c780
jason <jason.addie@gmail.com>
2015-08-13 16:43:12 +0400

    fix bug with wrong variable name

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -9,25 +9,25 @@ module DatasetsHelper
     elsif start_at.present?
       text << I18n.l(start_at, format: :day_first)
     elsif end_at.present?
-      text << I18n.l(start_at, format: :day_first)
+      text << I18n.l(end_at, format: :day_first)
     end
     return text
   end
 
   # get the correct url to use for explore dataset
-  # can either be admin dataset page or root dataset page 
+  # can either be admin dataset page or root dataset page
   def url_dataset_explore(dataset, options={})
     request.path == explore_data_dashboard_path(dataset) ? explore_data_show_path(dataset, options) : explore_dataset_path(dataset, options)
   end
 
   # get the correct url to use for dashboard dataset
-  # can either be admin dataset page or root dataset page 
+  # can either be admin dataset page or root dataset page
   def url_dataset_dashboard(dataset, options={})
     request.path == explore_data_show_path(dataset) ? explore_data_dashboard_path(dataset, options) : dataset_path(dataset, options)
   end
 
   # get the correct url to use on dashboard of dataset to send to dashboard of time series
-  # can either be admin dataset page or root dataset page 
+  # can either be admin dataset page or root dataset page
   def url_dataset_to_times_series_dashboard(dataset_id, time_series_id, options={})
     request.path == explore_data_dashboard_path(dataset_id) ? explore_time_series_dashboard_path(time_series_id, options) : time_series_path(time_series_id, options)
   end
