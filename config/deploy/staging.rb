75e04495b6bdd0d582c8ecfa5df735084669860b
jason <jason.addie@gmail.com>
2015-08-14 14:50:54 +0400

    fix deploy repo name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ set :user, "unicef-staging" # name of user on server
 set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
-set :github_repo_name, "X-Tractor" # name of git hub repo
+set :github_repo_name, "Xtractor" # name of git hub repo
 set :git_branch_name, "unicef-master" # name of branch to deploy
 set :rails_env, "staging" # name of environment: production, staging, ...
 ##################################
