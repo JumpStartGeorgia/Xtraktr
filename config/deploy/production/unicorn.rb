24383d5c70d8ec394e86d80b6d853f2f15ec6479
jason <jason.addie@gmail.com>
2015-08-28 15:19:25 +0400

    add prod deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,10 +1,10 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap/Bootstrap-Starter/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
-port_num = 8091 # must be a unique port number for this application
-tout = 30 # time in seconds before process dies - need a long time for data uploads
+root = "/home/xtraktr/Xtraktr/current" # path to application current folder
+sock_name = "unicorn_Xtraktr" # must be unique name with no '.'
+port_num = 8148 # must be a unique port number for this application
+tout = 180 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
@@ -14,5 +14,5 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 2
+worker_processes 5
 timeout tout
