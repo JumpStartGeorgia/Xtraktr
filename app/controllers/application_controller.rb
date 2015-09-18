cc0400f3be732afb52223e4b49057f7c3fca676b
Antarya <antarya@gmail.com>
2015-09-18 09:28:41 +0400

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -146,10 +146,12 @@ logger.debug "////////////////////////// BROWSER = #{@user_agent}"
      !(params[:controller] == 'omniauth_callbacks' && params[:action] == 'facebook') &&
       !(params[:controller] == 'settigns' && params[:action] == 'refill')
       session[:download_url] = nil
+         # Rails.logger.info "--------------------------- remove"
     end
 
     if params[:action] == 'download_request' && request.xhr? && (!user_signed_in? || !current_user.valid?)
       session[:download_url] = request.fullpath
+      # Rails.logger.info "--------------------------- added"
     end
 
     if request.fullpath.index("/download/").nil?
