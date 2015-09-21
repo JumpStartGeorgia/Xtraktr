f83b778f6606a8c03fb9bc5a3d73136aaa7506c9
jason <jason.addie@gmail.com>
2015-09-21 10:17:44 +0400

    update timeout to 5 mins

--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/unicef-staging/Unicef-Staging/current" # path to application current folder
 sock_name = "unicorn_Unicef-Staging" # must be unique name with no '.'
 port_num = 8140 # must be a unique port number for this application
-tout = 180 # time in seconds before process dies - need a long time for data uploads
+tout = 300 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
