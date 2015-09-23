82fca49ad13eb29d25af3c837f2e115090b7634f
Antarya <antarya@gmail.com>
2015-09-23 16:05:04 +0400

    sign in fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,15 @@ class Users::SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)    
+        if resource.valid?
+            respond_with resource, location: after_sign_in_path_for(resource)
+        else
+            respond_to do |format|
+            format.html { redirect_to after_sign_in_path_for(resource) }
+            format.json { render :json => {url: after_sign_in_path_for(resource)} }
+        end
+    end
+    #respond_with resource, location: after_sign_in_path_for(resource)    
    end
      def failure 
         render :json => { :errors => self.flash.to_hash, sessions: true }, :status => :error
