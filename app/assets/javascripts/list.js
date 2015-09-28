7493f1b179d12b185b89c23da6325a2e86ee4030
Antarya <antarya@gmail.com>
2015-09-28 17:39:38 +0400

    Updating loader

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -13,7 +13,12 @@ function filter () {
   if (category !== "none") {
     data["category"] = category;
   }
-  $("body").append("<div class='loading'></div>");
+  if(!$("body .cssloader").length) {
+    $("body").append(cssloader());
+  } 
+  else {
+    $("body .cssloader").show();
+  }
   $.ajax({
     url: url,
     data: data,
@@ -21,8 +26,15 @@ function filter () {
   }).done(function (d) {
     $(".list").html(d.d);
   }).always(function () {
-    $(".loading").fadeOut(500, function () { $(this).remove(); });
+    $(".cssloader").fadeOut(500, function () {  console.log(this);/*$(this).remove();*/ });
   });
+}
+
+
+
+
+function cssloader () {
+  return '<div class="cssloader"><svg version="1.1" width="200px" height="200px" viewBox="0 0 200 200" enable-background="new 0 0 200 200" xml:space="preserve"><g><path class="circle1" d="M138.768,100c0,21.411-17.356,38.768-38.768,38.768c-21.411,0-38.768-17.356-38.768-38.768c0-21.411,17.357-38.768,38.768-38.768"/><path class="circle2" d="M132.605,100c0,18.008-14.598,32.605-32.605,32.605c-18.007,0-32.605-14.598-32.605-32.605c0-18.007,14.598-32.605,32.605-32.605"/><path class="circle3" d="M126.502,100c0,14.638-11.864,26.502-26.502,26.502c-14.636,0-26.501-11.864-26.501-26.502c0-14.636,11.865-26.501,26.501-26.501"/><path  class="circle4" d="M120.494,100c0,11.32-9.174,20.494-20.494,20.494c-11.319,0-20.495-9.174-20.495-20.494c0-11.319,9.176-20.495,20.495-20.495"/></g></svg></div>';
 }
 
 $(document).ready(function () {
