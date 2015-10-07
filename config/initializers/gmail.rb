609a387a0efb3caacae129349cb3e772316c8706
jason <jason.addie@gmail.com>
2015-10-07 17:47:20 +0400

    fix email domain

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -2,7 +2,7 @@ if Rails.env.production? || Rails.env.staging?
 	ActionMailer::Base.smtp_settings = {
 		:address              => "smtp.gmail.com",
 		:port                 => 587,
-		:domain               => 'www.jumpstart.ge',
+		:domain               => 'gmail.com',
 		:user_name            => ENV['APPLICATION_FEEDBACK_FROM_EMAIL'],
 		:password             => ENV['APPLICATION_FEEDBACK_FROM_PWD'],
 		:authentication       => 'plain',
