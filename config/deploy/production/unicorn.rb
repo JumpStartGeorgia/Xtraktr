af692f9649e09f20fd79ee0a3a81c9e1032a0b72
jason <jason.addie@gmail.com>
2015-11-13 13:55:43 +0400

    update timeout for prod

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/data-app/Data-App/current" # path to application current folder
 sock_name = "unicorn_Data-App" # must be unique name with no '.'
 port_num = 8140 # must be a unique port number for this application
-tout = 300 # time in seconds before process dies - need a long time for data uploads
+tout = 1200 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
