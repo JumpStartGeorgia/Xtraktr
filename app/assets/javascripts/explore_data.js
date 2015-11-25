0baf8ff3c2601204afc7e5448102ef37e667c3b0
Antarya <antarya@gmail.com>
2015-11-25 10:31:25 +0400

    Api codebook method updated to return descriptive_statistics and answers based question type

--- app/assets/javascripts/explore_data.js
+++ app/assets/javascripts/explore_data.js
@@ -749,8 +749,7 @@ function get_explore_data (is_back_button) { // get data and load page
     .error(function ( jqXHR, textStatus, errorThrown ) {
       //console.log( "Request failed: " + textStatus  + ". Error thrown: " + errorThrown);
     })
-    .success(function ( json ) {
-       console.log(json);
+    .success(function ( json ) {       
       if (json.errors || ((json.results.analysis && json.results.analysis.length == 0) || json.results.filtered_analysis && json.results.filtered_analysis.length == 0)){
         $("#jumpto-loader").fadeOut("slow");
         $("#explore-data-loader").fadeOut("slow");
