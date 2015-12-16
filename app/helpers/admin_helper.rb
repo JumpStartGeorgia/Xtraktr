2a94465207864969cc403442fbe97f1d21059fc5
nwshane <django09@gmail.com>
2015-12-16 12:47:36 +0400

    Code style fix.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -7,7 +7,7 @@ module AdminHelper
     label = t("mongoid.attributes.#{model_i18n_key}.#{field}")
 
     unless I18n.default_locale == tab_locale
-      label << show_default_text(default_locale_field_value) 
+      label << show_default_text(default_locale_field_value)
     end
 
     label
