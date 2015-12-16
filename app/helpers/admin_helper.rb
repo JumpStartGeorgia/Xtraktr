ca052c5ab348a9d645bb076c0d13c7187392e95a
nwshane <django09@gmail.com>
2015-12-16 12:37:36 +0400

    Added admin_translation_label helper.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,10 @@ module AdminHelper
   def admin_translation_hint(model, field)
     t("mongoid.hints.#{model.model_name.i18n_key.to_s}.#{field}")
   end
+
+  def admin_translation_label(model_i18n_key, field, tab_locale, default_locale_field_value='')
+    label = t("mongoid.attributes.#{model_i18n_key}.#{field}")
+    label << default_locale_field_value unless I18n.default_locale == tab_locale
+    label
+  end
 end
