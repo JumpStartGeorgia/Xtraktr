696de39a3d7962d375c00c1381fc5f59373d715e
nwshane <django09@gmail.com>
2015-12-16 13:22:16 +0400

    Only displaying default locale field value in admin translation label if it is not blank.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -6,7 +6,9 @@ module AdminHelper
   def admin_translation_label(model_i18n_key, field, tab_locale, default_locale_field_value='')
     label = t("mongoid.attributes.#{model_i18n_key}.#{field}")
 
-    unless I18n.default_locale == tab_locale
+    # As a favor to users, show the default locale's value of the field on
+    # other locales (makes translation easier)
+    if I18n.default_locale != tab_locale && default_locale_field_value.present?
       label << show_default_text(default_locale_field_value)
     end
 
