3b2d472e4b791c737942ce44993d131351675d99
nwshane <django09@gmail.com>
2015-12-17 14:07:42 +0400

    Reorganized help article model code.

--- app/models/help_article.rb
+++ app/models/help_article.rb
@@ -4,26 +4,11 @@ class HelpArticle
   include Mongoid::Timestamps
 
   #############################
-  attr_accessible :title,
-                  :title_translations
-
   field :title, type: String, localize: true
   index(title: 1)
 
-  field :article_type, type: Integer, default: 1
-  index(article_type: 1)
-
-  ARTICLE_TYPES = {
-    how_to: 1,
-    tip: 2
-  }
-
-  def article_type_name
-    ARTICLE_TYPES.keys[ARTICLE_TYPES.values.index(article_type)]
-  end
-
-  #############################
-  # Validations
+  attr_accessible :title,
+                  :title_translations
 
   def validates_presence_of_title_for_default_language
     default_language = I18n.default_locale.to_s
@@ -37,4 +22,17 @@ class HelpArticle
                       msg: I18n.t('errors.messages.blank')))
   end
   validate :validates_presence_of_title_for_default_language
+
+  #############################
+  field :article_type, type: Integer, default: 1
+  index(article_type: 1)
+
+  ARTICLE_TYPES = {
+    how_to: 1,
+    tip: 2
+  }
+
+  def article_type_name
+    ARTICLE_TYPES.keys[ARTICLE_TYPES.values.index(article_type)]
+  end
 end
