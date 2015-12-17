3e265c44ec983947d10e1cd1ac810abc9c5d9400
Antarya <antarya@gmail.com>
2015-12-17 17:48:56 +0400

    Migration script changed, rounding weighted count in v3 done

--- db/migrate/update_dataset_questions_data_type.rb
+++ db/migrate/update_dataset_questions_data_type.rb
@@ -3,29 +3,21 @@
 
 puts "DATASETS - updating datasets questions data_type field, if question is categorical calculate frequency_data"
 Dataset.each do |d|
+  start = Time.now
   puts "updating #{d.title}"
   d.questions.each do |q|
-    items = d.data_items.with_code(q.code)
-    code_data = items.data
-
-    if q.has_code_answers
-      q.data_type = Question::DATA_TYPE_VALUES[:categorical]
-      # items.unset(:grouped_data) if field was removed but document had it.
-      frequency_data = {}
-      total = 0
-      q.answers.sorted.each {|answer|
-        cnt = code_data.select{|x| x == answer.value }.count
-        total += cnt
-        frequency_data[answer.value] = [cnt, (cnt.to_f/code_data.length*100).round(2)]
-      }
-      items.update_attributes({ frequency_data: frequency_data, frequency_data_total: total })          
-      q.is_analysable = q.answers.all_for_analysis.count > 0
-      q.has_data_without_answers = total < code_data.select{|d| !d.nil? }.length 
-    else 
-      q.has_data_without_answers = code_data.select{|d| !d.nil? }.length > 0
-      q.data_type = Question::DATA_TYPE_VALUES[:unknown]
-      q.is_analysable = false
+    if q.data_type == Question::DATA_TYPE_VALUES[:categorical]
+      d.update_question_type(q.code, q.data_type, nil)
+    elsif q.data_type == Question::DATA_TYPE_VALUES[:numerical]
+      d.update_question_type(q.code, q.data_type, q.numerical)
+    else
+      if q.has_code_answers
+        d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:categorical], nil)       
+      else 
+        d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:unknown], nil)       
+      end
     end
   end
   d.save
+  puts "it took #{Time.now-start} seconds to add the questions"  
 end
