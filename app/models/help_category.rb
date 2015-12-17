ebc6de4035a024134b2109d1e177b9777ef11190
nwshane <django09@gmail.com>
2015-12-17 10:46:17 +0400

    Code style fix.

--- app/models/help_category.rb
+++ app/models/help_category.rb
@@ -6,14 +6,13 @@ class HelpCategory
                   :name_translations,
                   :permalink,
                   :sort_order
-  #############################
 
+  #############################
   field :name, type: String, localize: true
   field :permalink, type: String
   field :sort_order, type: Integer, default: 1
 
   #############################
-  # indeces
   index ({ :permalink => 1})
   index ({ :name => 1})
   index ({ :sort_order => 1})
