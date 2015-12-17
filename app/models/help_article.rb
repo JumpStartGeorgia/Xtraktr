bcc2de4092ca185618aa5f3b17c8de92f1ade98a
nwshane <django09@gmail.com>
2015-12-17 12:58:43 +0400

    Added help article validation for presence of title in default language.

--- app/models/help_article.rb
+++ app/models/help_article.rb
@@ -10,4 +10,19 @@ class HelpArticle
   field :title, type: String, localize: true
   index(title: 1)
 
+  #############################
+  # Validations
+
+  def validates_presence_of_title_for_default_language
+    default_language = I18n.default_locale.to_s
+
+    return if title_translations[default_language].present?
+
+    errors.add(:base,
+               I18n.t('errors.messages.translation_default_lang',
+                      field_name: self.class.human_attribute_name('title'),
+                      language: Language.get_name(default_language),
+                      msg: I18n.t('errors.messages.blank')))
+  end
+  validate :validates_presence_of_title_for_default_language
 end
