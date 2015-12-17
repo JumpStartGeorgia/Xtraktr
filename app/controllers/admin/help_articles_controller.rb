9f0e8ae97ae004e08a21c215be0edc585641d18d
nwshane <django09@gmail.com>
2015-12-17 12:51:56 +0400

    Added a form to create new help articles.

--- app/controllers/admin/help_articles_controller.rb
+++ app/controllers/admin/help_articles_controller.rb
@@ -6,4 +6,14 @@ class Admin::HelpArticlesController < ApplicationController
       format.html
     end
   end
+
+  def new
+    @help_article = HelpArticle.new
+
+    set_tabbed_translation_form_settings
+
+    respond_to do |format|
+      format.html
+    end
+  end
 end
--- app/models/help_article.rb
+++ app/models/help_article.rb
@@ -1,9 +1,13 @@
+# Helps a site user understand XTraktr
 class HelpArticle
   include Mongoid::Document
   include Mongoid::Timestamps
 
   #############################
-  attr_accessible :title
+  attr_accessible :title,
+                  :title_translations
 
-  field :title, type: String
+  field :title, type: String, localize: true
+  index(title: 1)
+
 end
