c7658c878b56afc07894ad95637039201558866d
nwshane <django09@gmail.com>
2015-12-17 10:33:12 +0400

    Added indeces to help categories.

--- app/models/help_category.rb
+++ app/models/help_category.rb
@@ -1,12 +1,20 @@
 class HelpCategory
   include Mongoid::Document
 
+  #############################
   attr_accessible :name,
                   :name_translations,
                   :permalink,
                   :sort_order
+  #############################
 
   field :name, type: String, localize: true
   field :permalink, type: String
   field :sort_order, type: Integer, default: 1
+
+  #############################
+  # indeces
+  index ({ :permalink => 1})
+  index ({ :name => 1})
+  index ({ :sort_order => 1})
 end
