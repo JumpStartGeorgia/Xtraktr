e61304e4a7bdd4df267d442466688307203e4739
nwshane <django09@gmail.com>
2015-12-18 11:15:25 +0400

    Added update action to help articles controller.

--- app/controllers/admin/help_articles_controller.rb
+++ app/controllers/admin/help_articles_controller.rb
@@ -55,6 +55,26 @@ class Admin::HelpArticlesController < ApplicationController
     end
   end
 
+  def update
+    @help_article = HelpArticle.find(params[:id])
+
+    respond_to do |format|
+      if @help_article.update_attributes(params[:help_article])
+        format.html do
+          redirect_to admin_help_articles_path,
+                      flash: {
+                        success: t('app.msgs.success_updated',
+                                   obj: t('mongoid.models.help_article.one'))
+                      }
+        end
+      else
+        set_tabbed_translation_form_settings
+
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+
   def destroy
     @help_article = HelpArticle.find(params[:id])
     @help_article.destroy
