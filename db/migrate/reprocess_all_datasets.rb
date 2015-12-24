b721271bc7943a62eab6616491776f7ea67d0478
Antarya <antarya@gmail.com>
2015-12-24 17:58:17 +0400

    Reprocess all datafiles using new fixed r script

---
+++ db/migrate/reprocess_all_datasets.rb
@@ -0,0 +1,11 @@
+# data_type field was added to questions
+# update all questions to have categorical data_value where is_analysable is true
+
+puts "Reprocess datasets - data_items by new fixed schema and related fields"
+Dataset.each do |d|
+  puts "updating #{d.title}"
+
+  d.reprocess_file  
+  d.save
+
+end
