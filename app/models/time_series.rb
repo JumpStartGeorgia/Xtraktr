434b9ad2a4f64d90845f019d77162249cabbb682
Antarya <antarya@gmail.com>
2015-12-28 12:04:36 +0400

    CSV read without quote_char param

--- app/models/time_series.rb
+++ app/models/time_series.rb
@@ -329,10 +329,10 @@ class TimeSeries < CustomTranslation
   # so this will check for questions that chnaged and then call the callbacks
   def check_questions_for_changes
     if self.check_questions_for_changes_status == true
-      logger.debug ">>>>> time series check_questions_for_changes callback"
+      #logger.debug ">>>>> time series check_questions_for_changes callback"
       self.questions.each do |q|
         if q.changed?
-          logger.debug ">>>>> ---- #{q.text} changed!"
+          #logger.debug ">>>>> ---- #{q.text} changed!"
           q.trigger_all_callbacks
         end
       end
