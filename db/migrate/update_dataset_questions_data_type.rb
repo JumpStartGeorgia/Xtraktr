6d4cb5b57a820afe12a7bc7579f8aa5b955b7aac
jason <jason.addie@gmail.com>
2015-12-29 23:54:54 +0400

    bug fix - setting question flags

--- db/migrate/update_dataset_questions_data_type.rb
+++ db/migrate/update_dataset_questions_data_type.rb
@@ -9,17 +9,23 @@ Dataset.all.no_timeout.each do |d|
   puts "-----------------------"
   puts "updating #{d.title}"
   d.questions.each do |q|
+    print "-- #{q.code}"
     if q.data_type == Question::DATA_TYPE_VALUES[:categorical]
+      print " - categorical"
       d.update_question_type(q.code, q.data_type, nil)
     elsif q.data_type == Question::DATA_TYPE_VALUES[:numerical]
+      print " - numerical"
       d.update_question_type(q.code, q.data_type, q.numerical)
     else
+      print " - unknown"
       if q.has_code_answers
+        print " - has answers -> categorical"
         d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:categorical], nil)       
       else 
         d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:unknown], nil)       
       end
     end
+    puts ""
   end
   d.check_questions_for_changes_status = true
   d.save
