6bc52a2ea78821f9db16ad9721689a0ad84ae780
jason <jason.addie@gmail.com>
2016-01-26 00:46:49 +0400

    bug - take out spreadsheet check

--- db/migrate/update_dataset_questions_data_type.rb
+++ db/migrate/update_dataset_questions_data_type.rb
@@ -7,28 +7,23 @@ puts "DATASETS - updating datasets questions data_type field, if question is cat
 Dataset.all.no_timeout.each do |d|
   puts "-----------------------"
   puts "updating #{d.title}"
-  # only update files that are not spreadsheets since we do not know the data types in spreadsheets by default
-  if ['csv', 'ods', 'xls', 'xlsx'].include?(d.file_extension)
-    puts "-- spreadsheet -> skipping!"
-  else
-    d.questions.each do |q|
-      if q.data_type == Question::DATA_TYPE_VALUES[:categorical]
-        d.update_question_type(q.code, q.data_type, nil)
-      elsif q.data_type == Question::DATA_TYPE_VALUES[:numerical]
-        d.update_question_type(q.code, q.data_type, q.numerical)
-        q.exclude = true
-      else
-        if q.has_code_answers
-          d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:categorical], nil)       
-        else 
-          d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:unknown], nil)       
-        end
+  d.questions.each do |q|
+    if q.data_type == Question::DATA_TYPE_VALUES[:categorical]
+      d.update_question_type(q.code, q.data_type, nil)
+    elsif q.data_type == Question::DATA_TYPE_VALUES[:numerical]
+      d.update_question_type(q.code, q.data_type, q.numerical)
+      q.exclude = true
+    else
+      if q.has_code_answers
+        d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:categorical], nil)       
+      else 
+        d.update_question_type(q.code, Question::DATA_TYPE_VALUES[:unknown], nil)       
       end
     end
-    d.check_questions_for_changes_status = true
-    d.save
-    puts "it took #{Time.now-start} seconds to add the questions"  
   end
+  d.check_questions_for_changes_status = true
+  d.save
+  puts "it took #{Time.now-start} seconds to add the questions"  
 end
 
 puts ""
