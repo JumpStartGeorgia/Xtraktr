b50a6689993bffa5e1198297eac497b373d52172
jason <jason.addie@gmail.com>
2016-03-29 11:14:48 +0400

    bug fix - add missing ?

--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -45,7 +45,7 @@ class PageContent
   # if title or content are '', reset value to nil so fallback works
   def set_to_nil
     self.title_translations.keys.each do |key|
-      self.title_translations[key] = nil if !self.title_translations[key].nil && self.title_translations[key].empty?
+      self.title_translations[key] = nil if !self.title_translations[key].nil? && self.title_translations[key].empty?
     end
 
     self.content_translations.keys.each do |key|
