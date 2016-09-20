8269afcba9d2edcc6ffa7aae104a2caf291e8bf7
jason <jason.addie@gmail.com>
2016-09-20 15:08:07 +0400

    inc timeout

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/xtraktr/Xtraktr/current" # path to application current folder
 sock_name = "unicorn_Xtraktr" # must be unique name with no '.'
 port_num = 8148 # must be a unique port number for this application
-tout = 1200 # time in seconds before process dies - need a long time for data uploads
+tout = 1800 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
